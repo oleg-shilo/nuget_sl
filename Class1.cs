f96d131d525ee0c055bed9a8403e5f342ce9c652
oleg-shilo <oleg.shilo@gmail.com>
2020-08-16 01:03:29 +1000

    wewewwe

--- Class1.cs
+++ Class1.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace nuget_sl
 {
     public class Class1
     {
-    	public void Test()
-    	{
-    	}
+        public void Test()
+        {
+            Debug.Assert(false);
+            Console.WriteLine("dsfdsaf");
+        }
     }
 }
